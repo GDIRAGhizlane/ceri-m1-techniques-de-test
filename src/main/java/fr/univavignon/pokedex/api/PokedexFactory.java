41fe15d27065a9c59e34e8067efb4f991bfe2775
ghizlane <ghizlane.gdira@alumni.univ-avignon.fr>
2022-05-06 11:03:50 +0200

    Modifiction du fichier pom.xml et ajout de la classe PokedexFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -0,0 +1,12 @@
+package fr.univavignon.pokedex.api;
+
+public class PokedexFactory {
+	/*
+	IPokemonMetadataProvider metadataProvider;
+	IPokemonFactory pokemonFactory;
+	
+	public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
+		return ;
+	}
+	*/
+}
